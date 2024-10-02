4141f87a3546eb461b2167f2e08d37da85dc0b0d
codedit334 <oussama7elabdioui@gmail.com>
2024-10-02 14:34:42 +0100

    Create App component

---
+++ my-laravel-vue-app/resources/js/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <header>
+            <h1>My Dashboard</h1>
+            <nav>
+                <router-link to="/students">Students</router-link>
+                <router-link to="/teachers">Teachers</router-link>
+            </nav>
+        </header>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style>
+/* Add your styles here */
+header {
+    background: #f8f8f8;
+    padding: 20px;
+    text-align: center;
+}
+nav a {
+    margin: 0 15px;
+    text-decoration: none;
+    color: #333;
+}
+</style>
