802a4107c17c15d4d9f443e6f403d4bf494ac59c
codedit334 <oussama7elabdioui@gmail.com>
2024-10-02 16:41:41 +0100

    Add seeds

--- my-laravel-vue-app/database/seeders/UsersTableSeeder.php
+++ my-laravel-vue-app/database/seeders/UsersTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,6 +15,54 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Create some teachers
+        $teacher1 = User::create([
+            'name' => 'Teacher One',
+            'email' => 'teacher1@example.com',
+            'password' => bcrypt('password'), // Use bcrypt for hashing the password
+            'role' => 'teacher',
+            'teacher_id' => null, // Teachers don't have a teacher_id
+        ]);
+
+        $teacher2 = User::create([
+            'name' => 'Teacher Two',
+            'email' => 'teacher2@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'teacher',
+            'teacher_id' => null,
+        ]);
+
+        // Create some students and associate them with teachers
+        User::create([
+            'name' => 'Student One',
+            'email' => 'student1@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'student',
+            'teacher_id' => $teacher1->id, // Associate with Teacher One
+        ]);
+
+        User::create([
+            'name' => 'Student Two',
+            'email' => 'student2@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'student',
+            'teacher_id' => $teacher1->id, // Associate with Teacher One
+        ]);
+
+        User::create([
+            'name' => 'Student Three',
+            'email' => 'student3@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'student',
+            'teacher_id' => $teacher2->id, // Associate with Teacher Two
+        ]);
+
+        User::create([
+            'name' => 'Student Four',
+            'email' => 'student4@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'student',
+            'teacher_id' => $teacher2->id, // Associate with Teacher Two
+        ]);
     }
 }
