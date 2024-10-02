794cdbe9a141b8e12361519aef037b0ecacd2a01
codedit334 <oussama7elabdioui@gmail.com>
2024-10-02 16:36:23 +0100

    User model: add constrain

--- my-laravel-vue-app/app/Models/User.php
+++ my-laravel-vue-app/app/Models/User.php
@@ -41,4 +41,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function students()
+    {
+        return $this->hasMany(User::class, 'teacher_id');
+    }
+
+    // A student belongs to a teacher
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
 }
