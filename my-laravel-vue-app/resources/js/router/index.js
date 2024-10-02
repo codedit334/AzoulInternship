1b7c8e509d149a41a3646c514d247623f009d208
codedit334 <oussama7elabdioui@gmail.com>
2024-10-02 14:55:41 +0100

    refactor router

--- my-laravel-vue-app/resources/js/router/index.js
+++ my-laravel-vue-app/resources/js/router/index.js
@@ -1,23 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import StudentList from '../components/StudentList.vue';
 import TeacherList from '../components/TeacherList.vue';
+import Login from '../components/Login.vue';
 
 const routes = [
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login,
+    },
     {
         path: '/students',
         name: 'StudentList',
         component: StudentList,
+        meta: { requiresAuth: true }, // Protect this route
     },
     {
         path: '/teachers',
         name: 'TeacherList',
         component: TeacherList,
+        meta: { requiresAuth: true }, // Protect this route
     },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+// Route guard to check authentication
+router.beforeEach((to, from, next) => {
+    const isAuthenticated = !!localStorage.getItem('user'); // Check if user is authenticated
+    if (to.meta.requiresAuth && !isAuthenticated) {
+        next({ name: 'Login' });
+    } else {
+        next();
+    }
 });
 
 export default router;
