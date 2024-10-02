556876931cbca58b8b4c0658fc171e4c31af4800
codedit334 <oussama7elabdioui@gmail.com>
2024-10-02 13:49:37 +0100

    Edit routes:dashboard

--- my-laravel-vue-app/routes/web.php
+++ my-laravel-vue-app/routes/web.php
@@ -36,6 +36,8 @@ Route::get('/dashboard', function () {
 
 
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -51,10 +53,7 @@ Route::middleware('auth')->group(function () {
         ]);
     });
     
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
-    
+
 });
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
