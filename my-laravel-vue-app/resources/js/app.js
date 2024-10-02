586ac30e4fda9388ee3e0f84432dda9694f17d78
codedit334 <oussama7elabdioui@gmail.com>
2024-10-02 14:39:33 +0100

    Integrate router

--- my-laravel-vue-app/resources/js/app.js
+++ my-laravel-vue-app/resources/js/app.js
@@ -2,16 +2,20 @@ import './bootstrap';
 import '../css/app.css';
 
 import { createApp, h } from 'vue';
+
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+import { createRouter, createWebHistory } from 'vue-router';
 import StudentList from './components/StudentList.vue';
 import TeacherList from './components/TeacherList.vue';
 import App from './components/App.vue';
+import router from './router';
 
 const app = createApp(App);
 
+app.use(router);
 app.component('student-list', StudentList); // Register StudentList component
 app.component('teacher-list', TeacherList); // Register TeacherList component
 
