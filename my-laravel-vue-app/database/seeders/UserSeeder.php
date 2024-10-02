da1c633e547cec98cc7cbdd5166a07134ef612be
codedit334 <oussama7elabdioui@gmail.com>
2024-10-02 13:41:44 +0100

    Edit seeder

--- my-laravel-vue-app/database/seeders/UserSeeder.php
+++ my-laravel-vue-app/database/seeders/UserSeeder.php
@@ -14,6 +14,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            ['name' => 'Admin User', 'email' => 'admin@example.com', 'password' => bcrypt('password'), 'role' => 'admin'],
+            ['name' => 'Teacher User', 'email' => 'teacher@example.com', 'password' => bcrypt('password'), 'role' => 'teacher'],
+            ['name' => 'Student User', 'email' => 'student@example.com', 'password' => bcrypt('password'), 'role' => 'student'],
+        ]);
     }
 }
