55286b9f83fd94d10e1e1c60da288c035f5a2d7f
codedit334 <oussama7elabdioui@gmail.com>
2024-10-02 13:54:11 +0100

    Edit controler:Teacher

--- my-laravel-vue-app/app/Http/Controllers/TeacherController.php
+++ my-laravel-vue-app/app/Http/Controllers/TeacherController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class TeacherController extends Controller
 {
-    //
+    public function index()
+{
+    $students = Student::all(); // Get all students
+    return view('teacher.dashboard', compact('students'));
+}
+
+public function store(Request $request)
+{
+    // Validate and add a student
+}
+
+public function destroy($id)
+{
+    // Delete a student
+}
+
 }
