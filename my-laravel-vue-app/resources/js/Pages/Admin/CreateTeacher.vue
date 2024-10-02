7b80af94522822b143454d6d3780485b31072073
codedit334 <oussama7elabdioui@gmail.com>
2024-10-03 00:04:47 +0100

    Create techer page

---
+++ my-laravel-vue-app/resources/js/Pages/Admin/CreateTeacher.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Create Teacher</h1>
+    <form @submit.prevent="createTeacher">
+      <div>
+        <label>Name</label>
+        <input v-model="name" type="text" required />
+      </div>
+      <div>
+        <label>Email</label>
+        <input v-model="email" type="email" required />
+      </div>
+      <button type="submit">Create Teacher</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { Inertia } from '@inertiajs/inertia';
+
+export default {
+  name: 'CreateTeacher',
+  data() {
+    return {
+      name: '',
+      email: '',
+    };
+  },
+  methods: {
+    createTeacher() {
+      Inertia.post('/admin/store-teacher', { name: this.name, email: this.email });
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
