c53d3dae5fbe505e2f62af70c985412c9af448f2
codedit334 <oussama7elabdioui@gmail.com>
2024-10-03 00:05:27 +0100

    Create student page

---
+++ my-laravel-vue-app/resources/js/Pages/Admin/CreateStudent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Create Student</h1>
+    <form @submit.prevent="createStudent">
+      <div>
+        <label>Name</label>
+        <input v-model="name" type="text" required />
+      </div>
+      <div>
+        <label>Email</label>
+        <input v-model="email" type="email" required />
+      </div>
+      <button type="submit">Create Student</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { Inertia } from '@inertiajs/inertia';
+
+export default {
+  name: 'CreateStudent',
+  data() {
+    return {
+      name: '',
+      email: '',
+    };
+  },
+  methods: {
+    createStudent() {
+      Inertia.post('/admin/store-student', { name: this.name, email: this.email });
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
