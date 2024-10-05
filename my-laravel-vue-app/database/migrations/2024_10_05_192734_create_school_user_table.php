7678669b8d493a5a3c11a18663c1268e15a65172
codedit334 <oussama7elabdioui@gmail.com>
2024-10-05 20:28:56 +0100

    Fill school pivot

---
+++ my-laravel-vue-app/database/migrations/2024_10_05_192734_create_school_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('school_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // References the users table
+            $table->foreignId('school_id')->constrained()->onDelete('cascade'); // References the schools table
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('school_user');
+    }
+};
